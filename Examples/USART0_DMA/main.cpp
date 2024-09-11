bc8b4979ff0574d0941ec6aff276c2ba16bf665e
BMourit <bnmguy@gmail.com>
2024-09-11 00:18:51 -0700

    update example code to reflect latest changes

--- Examples/USART0_DMA/main.cpp
+++ Examples/USART0_DMA/main.cpp
@@ -5,10 +5,14 @@
 #include <cstdio>
 #include <cstdint>
 
+#include "gd32f303re.h"
+
+#include "CORTEX.hpp"
 #include "CEE.hpp"
 #include "GPIO.hpp"
 #include "AFIO.hpp"
 #include "RCU.hpp"
+#include "STARTUP.hpp"
 #include "PMU.hpp"
 #include "USART.hpp"
 #include "DMA.hpp"
@@ -29,12 +33,32 @@ uint8_t rxbuffer[RX_DATA_SIZE];
 volatile uint8_t tx_count = 0;
 volatile uint16_t rx_count = 0;
 
-// USART and DMA configuration structs
-usart::USART_Config usart_config;
-dma::DMA_Config dma_rxtx_config;
+__attribute__((constructor(101))) void premain() {
+    CORTEX_DEVICE.set_nvic_priority_group(cortex::Priority_Group::PRIO_GROUP_PRE4SUB0);
+    STARTUP_DEVICE.startup_init();
+}
 
-void init_clocks() {
-    RCU_DEVICE.clocks_init();
+extern "C" void USART0_IRQHandler(void) {
+    // Get USART0 instance
+    auto usart_result = usart::USART::get_instance(usart::USART_Base::USART0_BASE);
+    if (usart_result.error() != usart::USART_Error_Type::OK) {
+        handle_error("USART0 IRQ Handler Error");
+    }
+    usart::USART& usart = usart_result.value();
+
+    if (usart.get_flag(usart::Status_Flags::FLAG_RBNE)) {
+        // Read data from USART
+        uint8_t data = static_cast<uint8_t>(usart.receive_data() & 0xFF);
+
+        // Store data in RX buffer if space is available
+        if (rx_count < RX_BUFFER_SIZE) {
+            rx_count = rx_count + 1;
+            rxbuffer[rx_count] = data;
+        }
+
+        // Clear RX flag
+        //usart.clear_flag(usart::Status_Flags::FLAG_RBNE);
+    }
 }
 
 static usart::USART& init_usart(bool use_dma_rx, bool use_dma_tx) {
@@ -45,15 +69,18 @@ static usart::USART& init_usart(bool use_dma_rx, bool use_dma_tx) {
 
     usart::USART& usart = usart_result.value();
 
+    usart::USART_Config usart_config;
+    usart::USART_Pins pin_config;
+
     // USART0 pin configurations
-    usart::USART_Pin_Config rx_pin_params = {
+    usart::USART_Pin_Config rx_pin = {
         .gpio_port = gpio::GPIO_Base::GPIOA_BASE,
         .pin = gpio::Pin_Number::PIN_10,
-        .mode = gpio::Pin_Mode::INPUT_FLOATING,
+        .mode = gpio::Pin_Mode::INPUT_PULLUP,
         .speed = gpio::Output_Speed::SPEED_10MHZ,
     };
 
-    usart::USART_Pin_Config tx_pin_params = {
+    usart::USART_Pin_Config tx_pin = {
         .gpio_port = gpio::GPIO_Base::GPIOA_BASE,
         .pin = gpio::Pin_Number::PIN_9,
         .mode = gpio::Pin_Mode::ALT_PUSHPULL,
@@ -61,16 +88,19 @@ static usart::USART& init_usart(bool use_dma_rx, bool use_dma_tx) {
     };
 
     // Set USART configuration parameters
-    usart_config.rx_pin_config = rx_pin_params;
-    usart_config.tx_pin_config = tx_pin_params;
+    pin_config.rx_pin = rx_pin;
+    pin_config.tx_pin = tx_pin;
     usart_config.baudrate = BAUD_RATE;
-    usart_config.dma_pin_ops = use_dma_rx ? usart::USART_DMA_Config::DMA_RX : usart::USART_DMA_Config::DMA_NONE;
+    usart_config.dma_pin_ops = use_dma_rx ? usart::USART_DMA_Config::DMA_RX :  use_dma_tx  ? usart::USART_DMA_Config::DMA_TX : usart::USART_DMA_Config::DMA_NONE;
     usart_config.word_length = usart::Word_Length::WL_8BITS;
     usart_config.stop_bits = usart::Stop_Bits::STB_1BIT;
     usart_config.parity = usart::Parity_Mode::PM_NONE;
     usart_config.direction = usart::Direction_Mode::RXTX_MODE;
     usart_config.msbf = usart::MSBF_Mode::MSBF_MSB;
-    usart.configure(&usart_config);
+    usart.reset();
+    usart.clear_flag(usart::Status_Flags::FLAG_TC);
+    usart.pins_configure(pin_config);
+    usart.configure(usart_config);
 
     if (use_dma_rx || use_dma_tx) {
         // If DMA is used, initialize DMA here
@@ -79,6 +109,8 @@ static usart::USART& init_usart(bool use_dma_rx, bool use_dma_tx) {
         if (use_dma_rx) {
             wait_for_dma_transfer_complete(dma_dev);
         }
+    } else {
+        usart.set_interrupt_enable(usart::Interrupt_Type::INTR_RBNEIE, true);
     }
 
     return usart;
@@ -91,6 +123,8 @@ static dma::DMA& init_dma(usart::USART& usart, bool use_dma_rx, bool use_dma_tx)
     }
 
     dma::DMA& dma_dev = dma_result.value();
+
+    dma::DMA_Config dma_rxtx_config;
 
     if (use_dma_tx) {
         handle_error("DMA on TX pin is not supported in this example");
@@ -113,9 +147,9 @@ static dma::DMA& init_dma(usart::USART& usart, bool use_dma_rx, bool use_dma_tx)
             .direction = dma::Transfer_Direction::P2M,
         };
 
-        dma_dev.configure(dma::DMA_Channel::CHANNEL4, &dma_rxtx_config);
+        dma_dev.configure(dma::DMA_Channel::CHANNEL4, dma_rxtx_config);
         dma_dev.set_circulation_mode_enable(dma::DMA_Channel::CHANNEL4, false);
-        dma_dev.channel_enable(dma::DMA_Channel::CHANNEL4);
+        dma_dev.set_channel_enable(dma::DMA_Channel::CHANNEL4, true);
     }
 
     return dma_dev;
@@ -130,7 +164,7 @@ static void wait_for_dma_transfer_complete(dma::DMA& dma_dev) {
 static void usart_send_polling(usart::USART& usart, const char* data) {
     while (*data != '\0') {
         usart.send_data(static_cast<uint16_t>(*data++));
-        while (!usart.get_flag(usart::Status_Flags::FLAG_TBE)) {
+        while (!usart.get_flag(usart::Status_Flags::FLAG_TC)) {
             // Wait until transmission buffer is empty
         }
     }
@@ -145,27 +179,26 @@ static void handle_error(const char* error_message) {
     }
 }
 
-int main(void)
-{
-    init_clocks();
+int main() {
 
     // Initialize USART with DMA RX and non-DMA TX
-    usart::USART& usart = init_usart(true, false);
+    usart::USART& usart = init_usart(false, false);
 
-    // Output received data
-    printf("\n\r%s\n\r", rxbuffer);
+    NVIC_EnableIRQ(USART0_IRQn);
 
     // Transmit message using non-DMA USART (USART0 TX)
     usart_send_polling(usart, TX_MESSAGE);
+    while (rx_count < RX_DATA_SIZE) {
+        // Wait for all data to be received
+    }
 
+    // Output received data
+    printf("\n\r%s\n\r", rxbuffer);
     return 0;
 }
 
-#define UNUSED(x)   (void)(x)
-
-// Retarget printf to USART0
-int fputc(int ch, FILE *f)
-{
+// Retarget printf to USART0 using __io_putchar
+int __io_putchar(int ch) {
     // Get USART0 instance
     auto usart_result = usart::USART::get_instance(usart::USART_Base::USART0_BASE);
     if (usart_result.error() != usart::USART_Error_Type::OK) {
@@ -173,9 +206,8 @@ int fputc(int ch, FILE *f)
     }
     usart::USART& usart0 = usart_result.value();
 
-    UNUSED(f);
     usart0.send_data(static_cast<uint16_t>(ch));
-    while (!usart0.get_flag(usart::Status_Flags::FLAG_TBE)) {
+    while (!usart0.get_flag(usart::Status_Flags::FLAG_TC)) {
     }
     return ch;
 }
